a3b8d98314f288b25870e04a298f817ccd8348cd
송명진 <reki318@naver.com>
2017-03-01 02:54:12 +0900

    database

--- app.js
+++ app.js
@@ -2,98 +2,37 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var http = require("http");
-
-var badDustContition = 80;
-
 var bodyParser = require('body-parser');
-/*
 var config = require('./config/config');
+var i18n  = require('i18n');
+var MsTranslator = require("mstranslator");
 var parseString = require('xml2js').parseString;
+var langdetect  = require('langdetect');
+var extend = require("util-extend");
+var sortBy = require("sort-by");
+var by = require('sortby');
+var arraySort = require("array-sort");
+
+
+var lanuageDect = require('languagedetect');
+
+
+
+
 
 var parse = require('xml-parser');
 var inspect = require('util').inspect;
-*/
 
-app.get("/call",function(req,res)
-{
-  var request = require("request");
+var client = new MsTranslator({
+  api_key : "789a9cf5936f46d8bcb8b7df0ce41324"
+},true);
 
-  var fetch_options = {
-    url: "http://apis.skplanetx.com/weather/airquality/current?lon=120&lat=40&version=1",
-        headers: {
-        "x-skpop-userId": "reki318@naver.com" ,
-        "Accept": "application/json" ,
-        "Accept-Language" : "ko_KR",
-        "appKey": "16638033-8a90-3674-bf00-916c8d800a7a"
-    }
-  }
+var params = {
+  text: 'How\'s it going?'
+  , from: 'en'
+  , to: 'ja'
+};
 
-  request.get(fetch_options,function(error, response, body){
-    if (!error && response.statusCode == 200) {
-      var info  = JSON.parse(body);
-      console.log(info);
-
-      var dust = info.weather.dust;
-      var timeObservation = dust[0].timeObservation;
-      var value = dust[0].pm10.value;
-      var grade = dust[0].pm10.grade;
-
-      console.log("value :" + value + "\n" + "grade :" + grade);
-      var result = {};
-
-      result["value"] = value;
-      result["grade"] = grade;
-
-      res.end();
-    }
-});
-});
-
-app.get("/world",function(req,res){
-  var request = require('request');
-  var country = req.param('country');
-  var url = "http://api.waqi.info/feed/"+ country +"/?token=011ca88ea405df5b9438fd24239d2a150eeb6be9";
-
-  console.log(url);
-
-  request({
-      url: url ,
-      method: 'GET'
-  }, function (error, response, body) {
-       var info  = JSON.parse(body);
-       var dust = info.data.iaqi.pm10;
-       var a = dust.v;
-       console.log("pm10:" + a);
-
-      if(a < 30)
-      {
-        result["grade"] = "좋음";
-      }
-      else if (31 < a < 80)
-      {
-        result["grade"] = "보통";
-      }
-      else if (81 < a < 120)
-      {
-        result["grade"] = "약간 나쁨";
-      }
-      else if (121 < a < 200)
-      {
-        result["grade"] = "나쁨";
-      }
-      else {
-        result["grade"] = "매우 나쁨";
-      }
-
-       var result = {};
-       result["value"] = a;
-
-
-
-       res.json(result);
-       return;
-    });
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -103,6 +42,10 @@ app.get('/weather',function(req,res){
 var request = require('request');
 var url = 'http://www.kweather.co.kr/air/data/api/air_1hr_all2.xml';
 var result = "";
+
+client.translate(params, function(err, data) {
+  console.log(data);
+});
 
 request({
     url: url ,
@@ -128,6 +71,77 @@ app.get('/keyboard',function(req, res){
     console.log(data);
     res.end(data);
   })
+});
+
+app.get("/ranking",function(req,res)
+{
+  var result = [];
+  var object = {};
+  fs.readFile("userdata.json","utf8",function(err,data){
+    var userdata = JSON.parse(data);
+    var count  = Object.keys(userdata).length;
+
+    //console.log(parseInt("10"));
+
+    for(var i = 1 ; i<count+1;i++)
+    {
+      result[i-1] = userdata["count"+i];
+    }
+    console.log(arraySort(result,'user_SCORE'));
+    result = arraySort(result,'user_SCORE');
+
+    console.log("1등:" + result[count-1].user_ID); //1등
+
+    for(var i = 0 ; i<count ; i++)
+    {
+      console.log(count-i+"th:" + result[i].user_ID); //1등
+      object[count-i+"th"] = result[i].user_ID;
+    }
+    res.json(object);
+
+  });
+});
+
+
+
+
+
+app.get("/userdata",function(req,res){
+  var user_ID = req.param("user_ID");
+  var user_SCORE = req.param("user_SCORE");
+
+  var result = {};
+  fs.readFile("userdata.json","utf8",function(err,data){
+    //만약 userID가 일치 할 경우 덮어씌워야함
+    var userdata = JSON.parse(data); //userID와 같을 때
+    console.log("길이" + Object.keys(userdata).length);
+    var count  = Object.keys(userdata).length;
+
+    console.log(userdata["count"+count].user_ID);
+    //count = userdata[user_ID].count;
+
+    if(userdata["count"+count].user_ID != user_ID ) //인증을 거치지 않은 부분
+    {
+      ++count;
+      result["count"+count] = { "user_ID" : user_ID , "user_SCORE" : user_SCORE ,"count" : count};
+
+      var object  = extend(userdata,result);
+      console.log(object);
+
+      var writefile = JSON.stringify(object, null ,'\t');
+      fs.writeFile("userdata.json",writefile,"utf8",function(err,data){
+          result = 200;
+          res.json(result);
+          return;
+      });
+    }
+    else{
+      result["success"] = 0;
+      result["error"] = "EXISTED";
+      res.json(result);
+      return;
+    }
+});
 });
 
 
@@ -162,6 +176,8 @@ app.post('/friend',function(req,res){
     })
   });
 
+
+
 app.post('/message',function(req, res){
   var result = { };
   var user_key = req.param('user_key');
@@ -192,8 +208,193 @@ app.post('/message',function(req, res){
             }
           else
           {
+
             fs.readFile( __dirname + "/../data/message.json", 'utf8', function (err, data) {
               var word = content.split(":");
+
+              console.log(word.length);
+              console.log("word: "+word[1]);
+
+              if(content.search(":") == -1)
+              {
+                var message = {};
+
+              //  var ang = langdetect.detectOne(content);
+                var lng  = new lanuageDect();
+                var ang  = lng.detect(content,10);
+
+                var sol = JSON.stringify(ang);
+
+                console.log(ang);
+
+                //console.log("content:" + content);
+                //console.log("language:" + ang);
+
+                if(sol.search("ja") != -1 ) //일본어를 선택할 시
+                {
+                  console.log("일본어 켜짐");
+
+                  var client_id = 'LMBXnR_gI9Y0rT00oP1J';
+                  var client_secret = 'XfZbFFsYOf';
+
+                  var api_url = 'https://openapi.naver.com/v1/language/translate';
+                  var request = require('request');
+                  var options = {
+                      url: api_url,
+                      form: {'source':'ja', 'target':'ko', 'text': content },
+                      headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+                  };
+                    request.post(options, function (error, response, body) {
+                      if (!error && response.statusCode == 200) {
+                        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                        var a  = JSON.stringify(body);
+                        var find = a.search("translatedText");
+                        var start = a.indexOf( ":", find );
+                        var end  =  a.indexOf("}",start);
+                        var result = a.slice(start + 3, end -2);
+                        message ={};
+                        message["message"] = {"text" : result };
+                        var s = JSON.stringify(message);
+                        console.log(s);
+                        res.end(s);
+                        }
+                        else {
+                        //res.status(response.statusCode).end();
+                        console.log('error = ' + response.statusCode);
+
+                        var result = ":을 안붙이셨습니다. \n 양식에 맞게 :을 붙여주세요!"
+                        message["message"] = {"text" : result};
+                        res.json(message);
+
+                        }
+                    });
+                }
+                else if(sol.search("en") != -1 ) //영어를 선택할 시
+                {
+                  console.log("영어 켜짐");
+
+                  var client_id = 'LMBXnR_gI9Y0rT00oP1J';
+                  var client_secret = 'XfZbFFsYOf';
+
+                    var api_url = 'https://openapi.naver.com/v1/language/translate';
+                    var request = require('request');
+                    var options = {
+                      url: api_url,
+                      form: {'source':'en', 'target':'ko', 'text': content },
+                      headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+                    };
+                    request.post(options, function (error, response, body) {
+                      if (!error && response.statusCode == 200) {
+                        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                        var a  = JSON.stringify(body);
+                        var find = a.search("translatedText");
+                        var start = a.indexOf( ":", find );
+                        var end  =  a.indexOf("}",start);
+                        var result = a.slice(start + 3, end -2);
+                          message ={};
+                          message["message"] = {"text" : result };
+                        var s = JSON.stringify(message);
+                        console.log(s);
+                        res.end(s);
+
+                      } else {
+                        //res.status(response.statusCode).end();
+                        console.log('error = ' + response.statusCode);
+
+                        var result = ":을 안붙이셨습니다. \n 양식에 맞게 :을 붙여주세요!"
+                        message["message"] = {"text" : result};
+                        res.json(message);
+                  }
+                });
+              }
+                else if(ang == "ko")
+                {
+                  console.log("한국어 단어 켜짐");
+                  console.log("단어 선택");
+                var url = "http://opendict.korean.go.kr/api/search?key=15A4C53F5510FE3CBAEE3C96291C2FEE&q="+ encodeURIComponent(content);
+
+                console.log(url);
+
+                var message = {};
+                var output = '';
+                var object = "";
+
+                http.get(url,function(web){
+                  web.on('data',function(buffer){
+                    output += buffer;
+                  });
+                  web.on('end',function(){
+                     object = output.toString();
+                     var s = "";
+                     parseString(object, function (err, result) {
+                        s = result["channel"]["item"]; //xml로 파싱한 상태
+                     });
+                     s = JSON.stringify(s);
+
+                    var resultword = new Array(10);
+                    var resultde = new Array(10);
+                    var result = "";
+                    var a = 0;
+
+                    if(s == undefined)
+                    {
+                      console.log(s);
+                      var message ={};
+                       message["message"] = {"text" : "데이터가 존재하지 않습니다."};
+                       res.json(message);
+                       return;
+                    }
+                  else{
+                    while(a != -1)
+                  {
+                    var  i = 0;
+                    var word = s.search("word");
+                    console.log(word);
+                    a = word;
+                    var definition = s.indexOf("definition");
+
+                    var wordstart = s.indexOf("[",word);
+                    var wordend = s.indexOf("]",wordstart);
+                    var wordslice = s.slice(wordstart+2, wordend-1);
+                    resultword[i] = wordslice;
+
+                    s = s.replace("word","");
+                    s = s.replace(wordslice,"");
+
+                    var depstart = s.indexOf("[",definition);
+                    var depend = s.indexOf("]",depstart);
+                    var depslice = s.slice(depstart+2, depend-1);
+                    resultde[i] = depslice;
+
+                    s = s.replace("definition","");
+                    s = s.replace(depslice,"");
+                    result += "단어: " + resultword[0] + "\n" + "의미: " +resultde[0] + "\n";
+                    i++;
+                  }
+                    var message ={};
+                    message["message"] = {"text" : result};
+                    res.json(message);
+                  }
+                });
+              });
+            }
+                else{
+                var result = ":을 안붙이셨습니다. \n 양식에 맞게 :을 붙여주세요!"
+                message["message"] = {"text" : result};
+                res.json(message);
+                }
+              }
+
+              else if(word.length != 2 )
+              {
+                console.log(":을 하나 이상 더 붙이셨습니다. \n :을 하나만 붙여주세요!");
+                var message = {};
+
+                var result = ":을 하나 이상 더 붙이셨습니다. \n :을 하나만 붙여주세요!"
+                message["message"] = {"text" : result};
+                res.json(message);
+              }
+
 
               if(word[0] == "단어")
               {
@@ -287,9 +488,9 @@ app.post('/message',function(req, res){
                         })
                       })
               })
-            }
+             }
               else if(word[0] == "영한")
-              {
+               {
                 var url = "http://endic.naver.com/translateAPI.nhn?sLn=kr&_callback=window.__jindo2_callback+"+"."+"$2431&m=getTranslate&query="+encodeURIComponent(word[1])+"&sl=en&tl=ko";
                   console.log(url);
                   // var result = {};
@@ -320,7 +521,7 @@ app.post('/message',function(req, res){
                       })
                     })
               })
-            }
+             }
               else if (word[0] == "사전")
               {
                 var client_id = 'ImKP3ZrLsLwL6hBgVedd';
@@ -372,7 +573,7 @@ app.post('/message',function(req, res){
               }
 
               else if(word[0] == "문장")
-              {
+               {
                 console.log("문장 선택");
 
                 var url ="http://www.jobkorea.co.kr/Service/User/Tool/SpellCheckExecute?tBox="+ encodeURIComponent(word[1]);
@@ -429,7 +630,8 @@ app.post('/message',function(req, res){
                   });
                 })//http request
               }
-            })
+
+            });
         }
         })
     }
@@ -482,6 +684,6 @@ app.delete('/chat_room/:user_key',function(req, res){
 
 
 
-var server = app.listen("3000", function(){
+var server = app.listen(config.port, function(){
   console.log("Run at http://localhost:3000");
 });
